f540685d78649e0b99ebf471f52618d1ac787a79
torvalds <deandb2015@gmail.com>
2019-03-30 15:52:22 +0800

    Add sine implementations

--- fibonacci/execute.rb
+++ fibonacci/execute.rb
@@ -3,10 +3,10 @@ require 'benchmark'
 
 puts "------------- n is 10 -------------"
 n = 10
-Benchmark.bm do |x|
-  x.report { fib(n) }
-  x.report { fib_fast_1(n) }
-  x.report { fib_fast(n) }
+Benchmark.bm(20) do |x|
+  x.report("Recursion: ") { fib(n) }
+  x.report("With hash cache: ") { fib_fast(n) }
+  x.report("Iterator: ") { fib_fast_1(n) }
 end
 puts "------------- END -------------"
 =begin
@@ -17,20 +17,17 @@ puts "------------- END -------------"
    0.000008   0.000001   0.000009 (  0.000008)
 ------------- END -------------
 =end
-puts "------------- n is 50 -------------"
-puts "------------- fib(n) will waste to long time -------------"
-n = 50
-Benchmark.bm do |x|
+puts "------------- fib(n) will waste too much time -------------"
+n = 5000
+puts "------------- n is #{n} -------------"
+Benchmark.bm(20) do |x| # bmbm
   # x.report { fib(n) } # Annotate it
-  x.report { fib_fast_1(n) }
-  x.report { fib_fast(n) }
+  x.report("fib_fast") { fib_fast(n) }
+  x.report("fib_fast_1") { fib_fast_1(n) }
+  x.report("fib_fast_2") { fib_fast_2(n) }
+  x.report("fib_fast_3") { fib_fast_3(n) }
+  x.report("fib_fast_4") { fib_fast_4(n) }
+  x.report("fib_fast_5") { fib_fast_5(n) }
+  x.report("fib_fast_6") { fib_fast_6(n) }
 end
 puts "------------- END -------------"
-=begin
-------------- n is 50 -------------
-------------- fib(n) will waste to long time -------------
-       user     system      total        real
-   0.000010   0.000000   0.000010 (  0.000010)
-   0.000028   0.000001   0.000029 (  0.000028)
-------------- END -------------
-=end
--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,11 +1,12 @@
 # Fibonacci number
-
+# 传统递归(二分递归，由于每次都需要重新计算，损耗大量时间和空间，时间复杂度O(2^n)
 def fib(n)
   return n if n == 0 || n == 1
   fib(n-1) + fib(n-2)
 end
 
 # Optimize first time
+# 备忘录法，使用递归方法，但利用 Hash 来存储来避免重复计算子问题
 def fib_fast(n, mem = {})
   return n if n == 0 || n == 1
 
@@ -13,12 +14,25 @@ def fib_fast(n, mem = {})
 end
 
 # Optimize again
-def fib_fast_1(n)
+#
+def fib_fast_1(n) # O(n)
+  a, b = 0, 1
+  return a if n == 0
+  return b if n == 1
+  2.upto(n).each do |i|
+    b = a + b
+    a = b - a
+  end
+  b
+end
+
+# 迭代加法
+def fib_fast_2(n)
   return n if n == 0 || n == 1
   fib1 = 1
   fib0 = 0
   fib2 = 1
-  2.upto(n).to_a.each do |i|
+  2.upto(n).each do |i|
     fib2 = fib1+ fib0
     fib0 = fib1
     fib1 = fib2
@@ -26,3 +40,35 @@ def fib_fast_1(n)
 
   return fib2
 end
+
+# 利用数组先存储数字，直接返回结果，耗费空间
+# n 越大, 越明显(内存换速度)
+def fib_fast_3(n)
+  arr = [0, 1]
+  return arr[n] if n <= 1
+  2.upto(n).each do |i|
+    arr[i] = arr[i-1] + arr[i-2]
+  end
+  arr[n]
+end
+
+# 迭代加法
+def fib_fast_4(n)
+  a, b = 0, 1
+  return a if n == 0
+  return b if n == 1
+  2.upto(n).each do |i|
+    tem = b
+    b = a + b
+    a = tem
+  end
+  b
+end
+
+# 尾递归
+def fib_fast_5(first = 0, second = 1, n)
+  return first if n == 0
+  return second if n == 1
+  fib_fast_5(second, first + second, n-1);
+end
+
