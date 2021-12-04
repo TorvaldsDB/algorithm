b781bf54ac86d6ca9673183e9f30483b08ac37c5
Torvalds Du <torvalds@ekohe.com>
2021-12-04 22:03:43 +0800

    selection sort

--- sort/execute.rb
+++ sort/execute.rb
@@ -3,3 +3,4 @@ require_relative 'sort.rb'
 a = [2,1,3,2,5,23,5,2,1,34,251,31,4,2341,42,4,241,2]
 
 puts bubble(a).inspect
+puts selection(a).inspect
--- sort/sort.rb
+++ sort/sort.rb
@@ -1,9 +1,21 @@
-def bubble(array)
-  count = array.count
+def bubble(arr)
+  count = arr.count
   count.times do |i|
     (count - i - 1).times do |j|
-      array[j] > array[j + 1] && (array[j], array[j + 1] = array[j + 1], array[j])
+      arr[j] > arr[j + 1] && (arr[j], arr[j + 1] = arr[j + 1], arr[j])
     end
   end
-  array
+  arr
+end
+
+def selection(arr)
+  n = arr.length - 1
+  n.times do |i|
+    min_index = i
+    for j in (i + 1)..n
+      min_index = j if arr[j] < arr[min_index]
+    end
+    arr[i], arr[min_index] = arr[min_index], arr[i] if min_index != i
+  end
+  arr
 end
